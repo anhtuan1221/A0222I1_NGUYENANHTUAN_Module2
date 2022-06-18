cbc0bd98fcf708e4c8dbcbca41ea6bfeb4afeb6b
anhtuan1221 <lucto744@gmail.com>
2022-06-18 15:05:39 +0700

    up them

--- ss12_java_collection_framework/excercise/quanLy_sanPham_linkedList/ProductManager.java
+++ ss12_java_collection_framework/excercise/quanLy_sanPham_linkedList/ProductManager.java
@@ -1,29 +1,84 @@
 package ss12_java_collection_framework.excercise.quanLy_sanPham_linkedList;
 
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Scanner;
+import java.util.*;
 
-public class ProductManager {
-    public static List<Product> list = new LinkedList<>();
+public class ProductManager extends Product {
+    public List<Product> list = new LinkedList<>();
     public static Scanner scanner = new Scanner(System.in);
 
-    public void add(Product product) {
-        list.add(product);
+    public ProductManager() {
+        list.add(new Product(1, "tuan", 10));
+        list.add(new Product(400, "hai", 20));
+        list.add(new Product(10, "quang", 100));
+        list.add(new Product(20, "duc", 101));
     }
 
-    public void edit(){
-        System.out.println("Edit product");
+    public void display() {
+        System.out.println("List product: ");
+        for (Product p : list) {
+            System.out.println(p.toString());
+        }
+    }
+
+    public void addProduct() {
+        System.out.println("Add Product");
+        System.out.println("Add ID");
+        int id = Integer.parseInt(scanner.nextLine());
+        System.out.println("Add name ");
+        String name = scanner.nextLine();
+        System.out.println("Add price");
+        double price = Double.parseDouble(scanner.nextLine());
+        list.add(new Product(id, name, price));
+        System.out.println("add success");
+    }
+
+    public void removeProduct() {
+        System.out.println("REMOVE PRODUCT");
+        System.out.println("CHOOSE ID");
         int id = Integer.parseInt(scanner.nextLine());
         for (int i = 0; i < list.size(); i++) {
-            if(list.get(i).getId()==id){
-                System.out.println("Enter name ");
+            if (list.get(i).getId() == id) {
+                list.remove(i);
+                break;
+            }
+        }
+    }
+
+    public void edit() {
+        System.out.println("EDIT PRODUCT");
+        System.out.println("choose id");
+        int id = Integer.parseInt(scanner.nextLine());
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getId() == id) {
+                System.out.println("Enter Id ");
+                list.get(i).setId(Integer.parseInt(scanner.nextLine()));
+                System.out.println("Enter name");
                 list.get(i).setName(scanner.nextLine());
                 System.out.println("Enter price");
                 list.get(i).setPrice(Double.parseDouble(scanner.nextLine()));
-
+                break;
             }
         }
+    }
+
+    public void searchNameProduct() {
+        System.out.println("SEARCH NAME PRODUCT");
+        System.out.println("ENTER NAME");
+        String name = scanner.nextLine();
+        for (int i = 0; i < list.size(); i++) {
+            if (list.get(i).getName().equals(name)) {
+                System.out.println(list.get(i).toString());
+            }
+        }
+    }
+
+    public void sort(boolean isDESC) {
+        Collections.sort(list, new Comparator<Product>() {
+            @Override
+            public int compare(Product o1, Product o2) {
+                return (int) (isDESC ? (o2.getPrice() - o1.getPrice()) : (o1.getPrice() - o2.getPrice()));
+            }
+        });
     }
 
 }
---
+++ ss12_java_collection_framework/excercise/quanLy_sanPham_linkedList/ProductTest.java
@@ -0,0 +1,49 @@
+package ss12_java_collection_framework.excercise.quanLy_sanPham_linkedList;
+
+import java.util.Scanner;
+
+public class ProductTest {
+    public static void main(String[] args) {
+        ProductManager productManager = new ProductManager();
+        Scanner scanner = new Scanner(System.in);
+        int choose = -1;
+        while (choose != 0) {
+            System.out.println("1.ADD");
+            System.out.println("2.EDIT");
+            System.out.println("3.REMOVE");
+            System.out.println("4.DISPLAY");
+            System.out.println("5.SEARCH");
+            System.out.println("6.SORT");
+            System.out.println("0.EXIT");
+            System.out.println("select option ");
+            choose = Integer.parseInt(scanner.nextLine());
+            switch (choose) {
+                case 1:
+                    productManager.addProduct();
+                    break;
+                case 2:
+                    productManager.edit();
+                    break;
+                case 3:
+                    productManager.removeProduct();
+                    break;
+                case 4:
+                    productManager.display();
+                    break;
+                case 5:
+                    productManager.searchNameProduct();
+                    break;
+                case 6:
+                    int choice = Integer.parseInt(scanner.nextLine());
+                    boolean isDESC = choice != 1;
+                    productManager.sort(isDESC);
+                case 0 :
+                    System.out.println("Exit");
+                    break;
+                default:
+                    break;
+
+            }
+        }
+    }
+}
